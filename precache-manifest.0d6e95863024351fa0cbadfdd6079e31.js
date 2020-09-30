bcc71a15a6ba4949e07cbd22cf75d79449cfab4c
baclemen <clemens.s.bachmann@gmail.com>
2020-09-30 22:22:19 +0200

    Updates

---
+++ precache-manifest.0d6e95863024351fa0cbadfdd6079e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f484bc201e7f67b502e0d7a870a3e54d",
+    "url": "/cb_pen_drawingapp/index.html"
+  },
+  {
+    "revision": "16f41b62c8ff73857270",
+    "url": "/cb_pen_drawingapp/static/css/main.467b8bb4.chunk.css"
+  },
+  {
+    "revision": "77151d6728a056d93e4f",
+    "url": "/cb_pen_drawingapp/static/js/2.f5f9e6db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cb_pen_drawingapp/static/js/2.f5f9e6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f41b62c8ff73857270",
+    "url": "/cb_pen_drawingapp/static/js/main.eb6dfdcf.chunk.js"
+  },
+  {
+    "revision": "8107fb687b18a61f00db",
+    "url": "/cb_pen_drawingapp/static/js/runtime-main.62915b1c.js"
+  },
+  {
+    "revision": "8872ecb84ac45a16e73656f269e9a9a1",
+    "url": "/cb_pen_drawingapp/static/media/Timestroke.8872ecb8.svg"
+  }
+]);
